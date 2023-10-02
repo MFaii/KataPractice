aa4e78a51ccc039ac6313d0baa839922c87b7055
Maximiliano Failla <faillamaximiliano@gmail.com>
2023-10-02 20:20:33 -0300

    Bin to decimal

---
+++ 8-kyu/4-BinToDecimal.js
@@ -0,0 +1,9 @@
+/* Complete the function which converts a binary number (given as a string) to a decimal number. */
+
+function binToDec(bin) {
+  return parseInt(bin, 2);
+}
+
+console.log(binToDec("23"));
+console.log(binToDec("1010"));
+console.log(binToDec("10101010"));
